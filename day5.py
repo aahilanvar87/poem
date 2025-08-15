a4983a7055bdd1821ac397d7c0b0128a540ac73e
aahilanvar87 <aahilanvar87@gmail.com>
2025-08-15 15:32:28 +0530

    learn if/else staments

--- day5.py
+++ day5.py
@@ -1,7 +1,4 @@
-# if= do some code only if some condition is true
-#       Else do something else
-age = init(input('enter your age')
 
-if age >= 12:
-    print(you are now signed up)
+age= int(input('enter your age:'))
+
     
---
+++ day6.py
@@ -0,0 +1,7 @@
+# Do some code only IF some condition is True
+#       Else do something else
+
+age=int(input("enter your age: "))
+
+if age >= 12:
+    print("you are now a good boy")
